2ef409351485d34cf55a90a4200b4bedee14fac5
Ashraja941 <ashraja941@gmail.com>
2025-12-22 21:24:31 -0600

    added lazy

--- init.lua
+++ init.lua
@@ -7,3 +7,6 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 require 'keymaps'
+require 'lazy-bootstrap'
+
+require 'lazy-plugins'
---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,24 @@
+require('lazy').setup({
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
