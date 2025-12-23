8dc7bf8411fbc51cc4770f1e72e5cd710a095931
Ashraja941 <ashraja941@gmail.com>
2025-12-23 17:17:10 -0600

    changed colors to be modular

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,48 +1,43 @@
-function ColorMyPencils(color)
+function ColorSetBGNone()
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-
 end
 
-return {
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     config = function()
-    --         require("tokyonight").setup({
-    --             -- your configuration comes here
-    --             -- or leave it empty to use the default settings
-    --             style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-    --             transparent = true, -- Enable this to disable setting the background color
-    --             terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-    --             styles = {
-    --                 -- Style to be applied to different syntax groups
-    --                 -- Value is any valid attr-list value for `:help nvim_set_hl`
-    --                 comments = { italic = false },
-    --                 keywords = { italic = false },
-    --                 -- Background styles. Can be "dark", "transparent" or "normal"
-    --                 sidebars = "dark", -- style for sidebars, see below
-    --                 floats = "dark", -- style for floating windows
-    --             },
-    --         })
-    --     end
-    -- },
+local tokyonight = {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+                style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = true, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "day", -- style for sidebars, see below
+                    floats = "day", -- style for floating windows
+                },
+            })
 
-    -- {
-    --     "rose-pine/neovim",
-    --     name = "rose-pine",
-    --     config = function()
-    --         require('rose-pine').setup({
-    --             disable_background = false,
-    --         })
-    --
-    --         vim.cmd("colorscheme rose-pine")
-    --
-    --         -- ColorMyPencils()
-    --     end
-    -- },
+            vim.cmd("colorscheme tokyonight")
+        end
+    }
 
-    {
+local rosepine =    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = false,
+            })
+
+            vim.cmd("colorscheme rose-pine")
+
+            -- ColorMyPencils()
+        end
+    }
+
+local catpuccin = {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
@@ -91,12 +86,14 @@ return {
                         enabled = true,
                         indentscope_color = "",
                     },
-                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
                 },
             })
 
             -- setup must be called before loading
             vim.cmd("colorscheme catppuccin")
         end
-    },
-}
+    }
+
+return rosepine
+
+
