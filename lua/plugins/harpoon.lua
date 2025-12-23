9789faf508e61c730c207a4399572208b6eb2d0b
Ashraja941 <ashraja941@gmail.com>
2025-12-23 17:25:51 -0600

    added harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,44 @@
+
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",  
+  opts = {},            -- REQUIRED: this makes `harpoon:setup()` run automatically
+
+  config = function(_, opts)
+    local harpoon = require("harpoon")
+    harpoon:setup(opts)
+  end,
+
+  keys = {
+    {
+      "<leader>ha",
+      function() require("harpoon"):list():add() end,
+      desc = "Harpoon Add File"
+    },
+    {
+      "<C-e>",
+      function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
+      desc = "Harpoon Toggle Menu"
+    },
+    {
+      "<C-h>",
+      function() require("harpoon"):list():select(1) end,
+      desc = "Harpoon Select 1"
+    },
+    {
+      "<C-j>",
+      function() require("harpoon"):list():select(2) end,
+      desc = "Harpoon Select 2"
+    },
+    {
+      "<C-k>",
+      function() require("harpoon"):list():select(3) end,
+      desc = "Harpoon Select 3"
+    },
+    {
+      "<C-l>",
+      function() require("harpoon"):list():select(4) end,
+      desc = "Harpoon Select 4"
+    },
+  },
+}
