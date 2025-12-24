4ab2b6a2af34e9d3b13c82347dfe2de810e8f586
Ashraja941 <ashraja941@gmail.com>
2025-12-23 22:43:46 -0600

    changed oil.nvim border

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,14 +5,17 @@ return {
 	opts = {},
 
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			float = {
+				border = "rounded",
+				win_options = {
+					winblend = 0,
+				},
+			},
+		})
 
 		-- Keymaps for oil.nvim
 		vim.keymap.set("n", "<space>-", require("oil").toggle_float, { desc = "Open oil in floating window" })
 	end,
-	-- Optional dependencies
-	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
 }
