160e35483f7fd9fa26767c86605dc3ed4d7e6f8d
Ashraja941 <ashraja941@gmail.com>
2026-06-21 00:24:31 -0500

    refactor: installed Treesitter Text objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-context",
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		branch = "main",
 		lazy = false,
